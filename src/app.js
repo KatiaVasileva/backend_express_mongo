cb76af1a1afdfb73b52af5a7a96cec036336ffe1
KatiaVasileva <eka.vasileva@gmail.com>
2024-10-13 18:05:29 +0500

    server is launched

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const {
+  API_URL = "http://127.0.0.1",
+  PORT = 3005,
+  MONGO_URL = "mongodb://127.0.0.1:27017/myMDB",
+} = process.env;
+
+mongoose.connect(MONGO_URL).catch((error) => handleError(error));
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.status(200);
+    response.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
+});
